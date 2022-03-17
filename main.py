fd570af8ee3b6fd9acfb1ce3257ea9803e12b123
sakshamt133 <76595308+sakshamt133@users.noreply.github.com>
2022-03-17 22:59:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from torchvision.models import resnet18
 
 model = resnet18(pretrained=False)
 model.fc.out_features = utils.num_classes
+model = model.double()
 opti = torch.optim.Adam(model.parameters(), lr=0.001)
 lossFun = torch.nn.CrossEntropyLoss()
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     for epoch in range(utils.epochs):
         for batch in train_data.train_batch:
             imgs, labels = batch
-            output = model(imgs)
+            output = model(imgs.double())
             loss = lossFun(output, labels)
             print(f'for epoch {epoch} loss is {loss}')
             loss.backward()
